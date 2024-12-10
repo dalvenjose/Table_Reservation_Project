733c5714d7770b209141d09f3a0fc605a6ad8bec
DALVEN JOSE <75852439+dalvenjose@users.noreply.github.com>
2024-12-10 10:52:52 +0530

    Update settings.py

--- restpro/restpro/settings.py
+++ restpro/restpro/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=w0m@!421^j###wc!kq5#ffjaagn!m#jb=!^k57f$jols7($%3'
+SECRET_KEY =
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -131,8 +131,8 @@ LOGIN_REDIRECT_URL = 'home'  # Redirect to home after login
 LOGOUT_REDIRECT_URL = 'login'  # Redirect to login after logout
 
 
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_HOST = '
+EMAIL_PORT = 
+EMAIL_USE_TLS =
 EMAIL_HOST_USER = 
 EMAIL_HOST_PASSWORD = 
