0e30a900320d3db8e7a1a029e2ff92a9bedafc02
Dalven <dalvenjose1106@gmail.com>
2024-12-10 10:30:28 +0530

    First commit

--- restpro/restpro/settings.py
+++ restpro/restpro/settings.py
@@ -134,5 +134,5 @@ LOGOUT_REDIRECT_URL = 'login'  # Redirect to login after logout
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'dalvenjose1106@gmail.com'
-EMAIL_HOST_PASSWORD = 'ihwy nbhh cjls zsem'
+EMAIL_HOST_USER = 
+EMAIL_HOST_PASSWORD = 
